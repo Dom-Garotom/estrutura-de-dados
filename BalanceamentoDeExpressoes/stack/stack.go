a027e2d17fbf110ed23624b6ed2b941632a8c1ad
Gean Ferreira <geanoficial14@gmail.com>
2025-10-11 19:40:52 -0300

    Balanceamento de expressões : criando a função responsável por remover o elemento do topo da stack

--- BalanceamentoDeExpressoes/stack/stack.go
+++ BalanceamentoDeExpressoes/stack/stack.go
@@ -26,3 +26,14 @@ func (stack *Stack) AddToTop(value string) {
 
 	stack.LastIn = currentNode
 }
+
+/*RemoveFromTop é um função responsável por remover o elemento que fica localizado no topo da pilha de elementos.*/
+func (stack *Stack) RemoveFromTop() bool {
+	if stack.LastIn == nil {
+		fmt.Printf("\nNão foi possível remover o ultimo elemento da pilha porque a pilha  estava vazia")
+		return false
+	}
+
+	stack.LastIn = stack.LastIn.nextNode
+	return true
+}
