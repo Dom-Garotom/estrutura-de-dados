70262592a1cbec68648888bb1c33f29fa65f89f3
Gean Ferreira <geanoficial14@gmail.com>
2025-10-11 19:39:20 -0300

    Balanceamento de expressões : criando a função de adicionar um novo elemento no topo da Stack

--- BalanceamentoDeExpressoes/stack/stack.go
+++ BalanceamentoDeExpressoes/stack/stack.go
@@ -4,6 +4,25 @@ Ele oferece funções que ajudam no gerenciamento dessa estrutura de dados.
 */
 package stack
 
+import "fmt"
+
 type Stack struct {
 	LastIn *Node
+}
+
+/*
+AddToTop é um função responsável por adicionar um novo item a pilha de elementos.
+
+	Ela recebe uma string como parâmetro.
+*/
+func (stack *Stack) AddToTop(value string) {
+	if value == "" {
+		fmt.Printf("\nValores vazios são inválidos")
+		return
+	}
+
+	currentNode := NewNode(value)
+	currentNode.nextNode = stack.LastIn
+
+	stack.LastIn = currentNode
 }
